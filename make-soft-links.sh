aaa60793d074ebe0f84e3d19aa2d1cfe053582c8
Pierre "Pete" Fleurant <petefleurant@gmail.com>
2025-04-16 16:58:06 -0400

    Necessary links for running MAO management

---
+++ make-soft-links.sh
@@ -0,0 +1,18 @@
+  #!/bin/bash
+#
+# make-soft-links.sh
+#
+# 24 Mar 2025
+#  New WIndows 11 machine
+#
+# Necessary links for running MAO management
+#
+#
+cd $HOME
+ln -s E:\\Astronomy Astronomy
+ln -s  E:\\Astronomy\\AAVSO\\Reports\\AAVSO\ Reports AAVSO\ Reports
+ln -s G:\\Shared\ drives\\MAO\ Images\\Pete\ Fleurant MAO-Pete
+ln -s G:\\Shared\ drives\\MAO\ Images\\Pierre\ Fleurant MAO-Pierre
+ln -s G:\\Shared\ drives\\MAO\ Images\\AutoFlat MAO-AutoFlat
+
+  
